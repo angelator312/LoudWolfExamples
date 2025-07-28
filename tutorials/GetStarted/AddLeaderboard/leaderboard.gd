f77e25062a67972709e1111026d66c2a3f4095a3
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-07-28 15:17:27 +0300

    made examples for description of loud wolf docs tutorials/GetStarted

--- tutorials/GetStarted/AddLeaderboard/leaderboard.gd
+++ tutorials/GetStarted/AddLeaderboard/leaderboard.gd
@@ -1,14 +1,18 @@
 extends Control
+class_name LeaderboardScript
 
-const SCORE_ITEM = preload("res://tutorials/GetStarted/AddLeaderboard/score_item.tscn")
-@onready var container: VBoxContainer = $M/Container
+
+const SCORE_ITEM = preload("score_item.tscn")
+@onready var container: VBoxContainer = $M/V/Container
 @onready var player_name: LineEdit = %PlayerName
 @onready var score: LineEdit = %Score
+
 
 func _ready() -> void:
 	LoudWolf.configure_api_key("CsCNpHfeeI4Tvt3u8y8Du6z1PzrWwkak6Y6YyWxY")
 	LoudWolf.configure_game_id("examplegame1")
 	make_scores()
+
 
 func make_scores():
 	await LoudWolf.Scores.get_scores(0)
@@ -17,18 +21,19 @@ func make_scores():
 		add_item(sc,i)
 		i+=1
 
+
 func add_item(sc,i:int):
 	var e:=SCORE_ITEM.instantiate()
-	e.order_idx=i
+	e.order_idx=i+1
 	e.player_name=sc.player_name
 	e.score=sc.score
 	container.add_child(e)
-	container.move_child(e,-2)
+
 
 func delete_scores():
 	for e in container.get_children():
-		if e.name!="ContainerForAdd":
-			e.queue_free()
+		e.queue_free()
+
 
 func _on_add_score_pressed() -> void:
 	await LoudWolf.Scores.save_score(player_name.text,score.text)
--- tutorials/GetStarted/AddLeaderboard/score_item.gd
+++ tutorials/GetStarted/AddLeaderboard/score_item.gd
@@ -1,12 +1,17 @@
 extends Control
+class_name ScoreItem
+
+
 @export var order_idx:int:
 	set(e):
 		order_idx=e
 		$Order.text=str(e)+"."
+
 @export var player_name:String:
 	set(e):
 		player_name=e
 		$Name.text=e
+
 @export var score:float:
 	set(e):
 		score=e
